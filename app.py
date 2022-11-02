f8a16ce1811e8cd4f01d21f00d52c63739220f3c
Erik Larsson <erik.larsson@molflow.com>
2022-11-02 16:36:15 +0100

    Add Platform fetcher stack

--- app.py
+++ app.py
@@ -5,12 +5,22 @@ from aws_cdk import App
 from fetcher.l0_fetcher_stack import L0FetcherStack
 
 app = App()
+
 L0FetcherStack(
     app,
-    "L0FetcherStack",
-    output_bucket_name="mats-l0-raw",
+    "L0RACFetcherStack",
+    output_bucket_name="mats-l0-raw-rac",
     config_ssm_name="/rclone/l0-fetcher",
-    source_path="/pub/OPS/TM/",
+    source_path="/pub/OPS/TM/Level0/",
+    full_sync=False,
+)
+
+L0FetcherStack(
+    app,
+    "L0PlatformFetcherStack",
+    output_bucket_name="mats-l0-raw-platform",
+    config_ssm_name="/rclone/l0-fetcher",
+    source_path="/pub/OPS/TM/Level1A/Platform/",
     full_sync=False,
 )
 
